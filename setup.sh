00257986aea0e2bc0acb6ccb3cd4ee16ab1eba5a
Patrick Huarng <huarngpa@gmail.com>
2022-06-25 21:50:41 -0400

    Fix deploy and setup scripts

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+echo "=== Creating partitioned topics ==="
 docker exec -it pulsar bin/pulsar-admin topics create-partitioned-topic -p 1 persistent://public/default/orders
 docker exec -it pulsar bin/pulsar-admin topics create-partitioned-topic -p 1 persistent://public/default/users
 docker exec -it pulsar bin/pulsar-admin topics create-partitioned-topic -p 1 persistent://public/default/items
@@ -6,10 +7,13 @@ docker exec -it pulsar bin/pulsar-admin topics create-partitioned-topic -p 1 per
 #docker exec -it pulsar bin/pulsar-admin topics create persistent://public/default/users
 #docker exec -it pulsar bin/pulsar-admin topics create persistent://public/default/items
 
+echo "=== Listing topics ==="
 docker exec -it pulsar bin/pulsar-admin topics list public/default
 
+echo "=== Setting topic retention ==="
 docker exec -it pulsar bin/pulsar-admin topics set-retention -s -1 -t -1 persistent://public/default/users
 docker exec -it pulsar bin/pulsar-admin topics set-retention -s -1 -t -1 persistent://public/default/items
 
+echo "=== Getting topic retention ==="
 docker exec -it pulsar bin/pulsar-admin topics get-retention persistent://public/default/users
 docker exec -it pulsar bin/pulsar-admin topics get-retention persistent://public/default/items
